3335e6df980627a5bee135c8ce0761fbbc181ef2
keito-isurugi <62861107+keito-isurugi@users.noreply.github.com>
2022-05-03 18:16:08 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+let bool: boolean = true;
+
+let num: number = 1;
+
+let str: string = "hoge";
+
+let ary: Array<number> = [0, 1, 2];
+let ary2: Array<string> = ["hoge", "foo"];
+let ary3: number[] = [0, 1, 2];
+
+let tuple: [number, string] = [1, "hoge"];
+
+let any: any = false;
+
+const funcA = (): void => {
+  const test = "TEST";
+};
+
+let null1: null = null;
+
+let undefined1: undefined = undefined;
+
+let obj1: object = { num: 1 };
+let obj2: { id: number; name: string } = { id: 1, name: "hoge" };
