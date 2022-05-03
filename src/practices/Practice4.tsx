82064b6550878f787e6d08d9ffb403ebb9776408
keito-isurugi <62861107+keito-isurugi@users.noreply.github.com>
2022-05-03 18:36:40 +0900

    練習4まで完了

--- src/practices/Practice4.tsx
+++ src/practices/Practice4.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export const Practice1 = () => {
-  const calcTotalFee = (num: number) => {
+export const Practice4 = () => {
+  const calcTotalFee = (num) => {
     const total = num * 1.1;
     console.log(total);
   };
   const onClickPractice = () => calcTotalFee(1000);
   return (
     <div>
-      <p>練習問題：引数の型指定</p>
-      <button onClick={onClickPractice}>練習問題1</button>
+      <p>練習問題：設定ファイル</p>
+      <button onClick={onClickPractice}>練習問題4</button>
     </div>
   );
 };
